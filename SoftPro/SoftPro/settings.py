6f889dc448ff3f7486f87c247ad1feca4ba5b270
Red <irina.purgina@inbox.ru>
2024-08-16 17:32:24 +0300

    init apps and add docker-compose

--- SoftPro/SoftPro/settings.py
+++ SoftPro/SoftPro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'httpapp.apps.HttpappConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +77,41 @@ WSGI_APPLICATION = 'SoftPro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sports_lines_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '54322',
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'sports_lines_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES['default']['NAME'] = 'test_' + DATABASES['default']['NAME']
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
---
+++ SoftPro/grpcapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GrpcappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'grpcapp'
